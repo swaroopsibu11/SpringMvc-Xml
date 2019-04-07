df0cbb91252d438cac20261ed0139a9772dc00c5
unknown <swaroopsibu11>
2019-04-08 00:07:33 +0530

    added simplet

---
+++ SpringMvcSimple/src/main/java/com/spring/controller/HelloController.java
@@ -0,0 +1,32 @@
+package com.spring.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController 
+{
+	@RequestMapping("/hello")  
+    public String display(HttpServletRequest req,Model m)  
+    {  
+        //read the provided form data  
+        String name=req.getParameter("name");  
+        String pass=req.getParameter("pass");  
+        if(pass.equals("banking"))  
+        {  
+            String msg="Hello "+ name;  
+            //add a message to the model  
+            m.addAttribute("message", msg);  
+            return "viewpage";  
+        }  
+        else  
+        {  
+            String msg="Sorry "+ name+". It's incorrect password";  
+            m.addAttribute("message", msg);  
+            return "errorpage";  
+        }     
+    }  
+}
